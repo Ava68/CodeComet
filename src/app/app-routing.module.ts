49d5bb43bd481540b0f5416f523ac448bce8e16d
avsingh <avadhesh.singh@greenwayhealth.com>
2024-01-16 16:49:01 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
+import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
+import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent
+  },
+  // defining link for edit category
+  {
+    path: 'admin/categories/:id',
+    component: EditCategoryComponent
+  },
+  {
+    path: 'admin/blogposts',
+    component: BlogpostListComponent
+  },
+  {
+    path: 'admin/blogposts/add',
+    component: AddBlogpostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
